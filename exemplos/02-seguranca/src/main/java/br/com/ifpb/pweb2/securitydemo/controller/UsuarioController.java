cea2cc560f629ef1c1b341b1d9635d791020dd28
isleimar <isleimar@gmail.com>
2019-10-19 11:35:51 -0300

    Refatorando o Usuario Controller

--- exemplos/02-seguranca/src/main/java/br/com/ifpb/pweb2/securitydemo/controller/UsuarioController.java
+++ exemplos/02-seguranca/src/main/java/br/com/ifpb/pweb2/securitydemo/controller/UsuarioController.java
@@ -2,8 +2,12 @@ package br.com.ifpb.pweb2.securitydemo.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,11 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ifpb.pweb2.securitydemo.domain.Usuario;
-import br.com.ifpb.pweb2.securitydemo.service.UsuarioException;
 import br.com.ifpb.pweb2.securitydemo.service.UsuarioService;
 
 @RestController
-@RequestMapping(path = "/api")
+@RequestMapping(path = "/usuarios")
 public class UsuarioController {
 	
 	private UsuarioService usuarioService;
@@ -24,25 +27,35 @@ public class UsuarioController {
 	public UsuarioController(UsuarioService usuarioService) {
 		this.usuarioService = usuarioService;
 	}
+	
+	@GetMapping("/{login}")
+    public Usuario recuperarPorLogin(@PathVariable("login") String login) {
+        return usuarioService.recuperarPorLogin(login).orElseThrow(RuntimeException::new);
+    }
 
-	@GetMapping("/usuarios")
+	@GetMapping
 	public List<Usuario> listarUsuarios(){
 		return usuarioService.listarUsuarios();
 	}
 	
-	@PostMapping("/usuario")
-	public Usuario adicionarUsuario(@RequestBody  Usuario usuario) throws UsuarioException {
-		return usuarioService.salvarUsuario(usuario);		
+	@PostMapping
+	public ResponseEntity<Usuario> adicionarUsuario(@RequestBody @Valid Usuario usuario){
+		try {
+			usuario = usuarioService.salvarUsuario(usuario);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().header("erro",e.getMessage()).build();
+		}
+		return ResponseEntity.ok(usuario);
 	}
 	
-	@PutMapping("/usuario")
-	public Usuario editarUsuario(@RequestBody Usuario usuario) {
+	@PutMapping
+	public Usuario editarUsuario(@RequestBody @Valid Usuario usuario) {
 		return usuarioService.atualizarUsuario(usuario);
 	}
 	
-	@DeleteMapping("/usuario")
-	public void excluirUsuario(@RequestBody Usuario usuario) {
-		usuarioService.removerUsuario(usuario.getId());
+	@DeleteMapping("/{id}")
+	public void excluirUsuario(@PathVariable("id") Long id) {
+		usuarioService.removerUsuario(id);
 	}
 
 }
