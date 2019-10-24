7308ca2b670245253158c80f1a65522f1f21d2d5
MailsonD <mailssondennis@gmail.com>
2019-10-24 11:03:29 -0300

    Iniciando endpoint de login

---
+++ exemplos/02-seguranca/src/main/java/br/com/ifpb/pweb2/securitydemo/controller/LoginController.java
@@ -0,0 +1,7 @@
+package br.com.ifpb.pweb2.securitydemo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+}
---
+++ exemplos/02-seguranca/src/main/java/br/com/ifpb/pweb2/securitydemo/controller/dto/LoginDTO.java
@@ -0,0 +1,11 @@
+package br.com.ifpb.pweb2.securitydemo.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+
+    private String login;
+    private String password;
+
+}
