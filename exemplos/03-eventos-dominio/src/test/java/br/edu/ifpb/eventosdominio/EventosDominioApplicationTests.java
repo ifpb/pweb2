934337d7abeeb4142334e0aba051a053c998c90f
Diego Pessoa <diegopessoa12@gmail.com>
2019-11-01 19:13:39 -0300

    Adiciona coleção do postman

---
+++ exemplos/03-eventos-dominio/src/test/java/br/edu/ifpb/eventosdominio/EventosDominioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.eventosdominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosDominioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
