e2f7aea320cdc3e666ae647f8606e5c3cf1c80ab
Diego Pessoa <diegopessoa12@gmail.com>
2019-11-25 19:27:24 -0300

    Exemplos da aula de comunicação assíncrona

---
+++ exemplos/comunicacao-assincrona/src/main/java/br/edu/ifpb/comunicacaoassincrona/ComunicacaoAssincronaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.comunicacaoassincrona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunicacaoAssincronaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComunicacaoAssincronaApplication.class, args);
+    }
+
+}
---
+++ exemplos/comunicacao-assincrona/src/test/java/br/edu/ifpb/comunicacaoassincrona/ComunicacaoAssincronaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.comunicacaoassincrona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunicacaoAssincronaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
